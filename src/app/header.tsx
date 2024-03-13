62b63ab87180a86d9524cbd401e5fb629a7f402b
judydev <>
2024-03-12 20:50:02 -0700

    fix build

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import styles from "./page.module.css";
-import Image from "next/image";
-import avatar from '../assets/avatar.png';
+// import Image from "next/image";
+// import avatar from '../assets/avatar.png';
 
 export default function Header() {
     return (
         <Link href="/">
-            <Image
+            {/* <Image
                 src={avatar}
                 width={210} height={200}
                 alt="Avatar"
                 style={{marginLeft: 20}}
-            /> 
+            />  */}
             <div className={styles.header}>Judy Dev</div>
             <hr />
             {/* TODO: add language button */}
